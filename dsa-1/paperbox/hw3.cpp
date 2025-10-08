474847f8e609f99f43ac7c44a88a31040a8b31ef
Blank <zhizhang808@gmail.com>
2025-10-08 09:29:18 +0800

    新增 hw3.cpp 實現 CppArray 類別，包含動態記憶體分配及運算子重載功能；更新 test.cpp 以計算嵌套迴圈的執行次數

---
+++ dsa-1/paperbox/hw3.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+const int defaultSize = 5;
+const float defaultValue = 1.0f;
+
+class CppArray {
+    public:
+
+    CppArray (int size = defaultSize, float initValue = defaultValue) {//建立陣列
+        arr_size = size;
+        if(size <= 0) {
+            cout << "size can not be zero" << endl;
+        }
+        cout << "constructor execute" << endl;
+        cpp_array = new float[size];
+        for (int i = 0; i < size; i++) {
+            cpp_array[i] = initValue;
+        }
+    }
+
+    CppArray (const CppArray& cp2) {
+        arr_size = cp2.arr_size;
+        cpp_array = new float [arr_size];
+        for (int i = 0; i < arr_size; i++) {
+            cpp_array[i] = cp2.cpp_array[i];
+        }
+    }
+
+    CppArray& operator = (const CppArray& cp2) {
+        if(this == &cp2) {
+            return *this;
+        }
+        delete[] cpp_array;
+        arr_size = cp2.arr_size;
+        cpp_array = new float[arr_size];
+       
+        for (int i =0; i < arr_size; i++) {
+            cpp_array[i] = cp2.cpp_array[i];
+        }
+        return *this;
+    }
+
+    float& operator[] (int i) {
+        if(i >= arr_size) {
+            cout << "too big" << endl;
+        }
+        else if (i < 0) {
+            cout << "too small" << endl;
+        }
+        return cpp_array[i];
+    }
+
+    friend istream& operator >> (istream& is, CppArray& cp2);
+    friend ostream& operator << (ostream& os, CppArray& cp2);
+
+    int GetSize() {
+        return arr_size;
+    }
+
+    ~CppArray() {
+        cout << "destructor execute" << endl;
+        delete[] cpp_array;
+    }
+
+    private:
+    float* cpp_array;
+    int arr_size;
+
+
+};
+
+ostream& operator << (ostream& os, CppArray& cp2) {
+    for (int i = 0; i < cp2.arr_size; i++) {
+        os << cp2.cpp_array[i];
+        if (i < cp2.arr_size-1) {
+            os << ", ";
+        }
+    }
+    os << endl;
+    return os;
+}
+
+int main() {
+
+    CppArray cp1;
+    CppArray cp2(3, 3);
+    cout << cp1.GetSize() << endl;
+    cp1 = cp2;
+    cp1.operator= (cp2);
+    cout << cp1.GetSize() << endl;
+    cout << cp1[1] << endl;
+    cout << cp2;
+    
+    return 0;
+}
--- dsa-1/paperbox/test.cpp
+++ dsa-1/paperbox/test.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main() {
-
-    int a =strlen("Michael Jordan");
-    cout << a;
+    int x = 0;
+    for(int i = 1;i <= 2;i++) {
+        for(int j = 1;j<=i;j ++) 
+            for(int k = 1;k <= j;k++)
+                x++;
+    }
+    cout << x;
 }
