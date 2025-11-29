b6f2865524751b680a84b21adcbf10eca1a1e16b
Blank <zhizhang808@gmail.com>
2025-11-29 17:10:49 +0800

    刪除 hw3.cpp 和 dsa-2025-2026-private 子專案，並新增 hw04_B1329008.cpp 檔案以實作多項式相乘功能

--- dsa-1/paperbox/HW03_B1329008.cpp
+++ dsa-1/paperbox/HW03_B1329008.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 const int defaultSize = 5;
-const float defaultValue = 1.0f;
+const float defaultValue = 3.14f;
 
 class CppArray {
     public:
 
     CppArray (int size = defaultSize, float initValue = defaultValue) {//建立陣列
-        arr_size = size;
+        
         if(size <= 0) {
             cout << "size can not be zero" << endl;
+            arr_size = defaultSize;
         }
         cout << "constructor execute" << endl;
-        cpp_array = new float[size];
+        arr_size = size;
+        cpp_array = new float[arr_size];
         for (int i = 0; i < size; i++) {
             cpp_array[i] = initValue;
         }
@@ -72,7 +74,7 @@ class CppArray {
 
 ostream& operator << (ostream& os, CppArray& cp2) {
     cout << "[";
-    for (int i = 0; i < cp2.arr_size; i++) {
+    for (int i = 0; i < cp2.arr_size; i++) {  
         os << cp2.cpp_array[i];
         if (i < cp2.arr_size-1) {
             os << ", ";
@@ -94,10 +96,10 @@ int main() {
     CppArray cp1;
     CppArray cp2(3, 3);
     cout << cp1.GetSize() << endl;
+    cout << cp1;
     cp1 = cp2;
     cout << cp1.GetSize() << endl;
     
-    cout << cp2;
     cout << cp2;
     try {
         cout << cp1[4] << endl;
---
+++ dsa-1/paperbox/hw04_B1329008.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int coef;
+    int exp;
+    ListNode *next;
+    ListNode (int x, int y) {
+        coef = x;
+        exp = y;
+        next = nullptr;
+    }
+};
+
+ListNode* multiply (ListNode* poly1, ListNode* poly2) {
+    ListNode* temp = new ListNode (poly1 -> coef * poly2 -> coef, poly1 -> exp + poly2 -> exp);
+    if (temp -> coef == 0) {//防輸入係數0
+        return nullptr;
+    }
+    else {
+        return temp;
+    }
+}
+
+int main () {
+    int term1 = 0;
+    int term2 = 0;
+    ListNode* temp1;
+    ListNode* poly1_head = new ListNode(0, 0);
+    poly1_head -> next = poly1_head;
+    ListNode* poly2_head = new ListNode(0, 0);
+    poly2_head -> next = poly2_head;
+    ListNode* poly_temp_head = new ListNode(0, 0);//相乘後的暫存node
+    ListNode* poly_end = new ListNode (0, 0);//最後答案的頭node
+    poly_end -> next = poly_end;
+
+    cout << "cin term1\n";
+    cin >> term1;
+    temp1 = poly1_head;
+    for (int i = 0; i < term1; i++) {
+        int coef = 0;
+        int exp = 0;
+        cout << "cin coef\n";
+        cin >> coef;
+        cout << "cin exp\n";
+        cin >> exp;
+        ListNode* poly1 = new ListNode(coef, exp);
+        temp1 -> next = poly1;
+        poly1 -> next = poly1_head;
+        temp1 = poly1;
+    }
+    cout << "cin term2\n";
+    cin >> term2;
+    temp1 = poly2_head;
+    for (int i = 0; i < term2; i++) {
+        int coef = 0;
+        int exp = 0;
+        cout << "cin coef\n";
+        cin >> coef;
+        cout << "cin exp\n";
+        cin >> exp;
+        ListNode* poly2 = new ListNode(coef, exp);
+        temp1 -> next = poly2;
+        poly2 -> next = poly2_head;
+        temp1 = poly2;
+    }
+
+    ListNode* temp2;
+    ListNode* temp_end = poly_end;
+    ListNode* temp3;
+
+    temp1 = poly1_head -> next;
+    
+    
+    
+    while (temp1 != poly1_head) {
+        ListNode* pBIG = poly_end;
+        temp2 = poly2_head -> next;
+        temp3 = poly_end;
+        while (temp2 != poly2_head) {
+            ListNode* multi = multiply(temp1, temp2);
+            if (multi == nullptr) {
+                continue;
+            }
+            else if (poly_end -> next == poly_end) {
+                poly1_head -> next = multi;
+                multi -> next = poly1_head;
+                temp3 = multi;
+            }
+            else if (multi -> exp > temp3 -> exp) {
+                pBIG -> next = multi;
+                multi -> next = temp3;
+                temp3 = multi;
+            }
+            else if (multi -> exp == temp3 -> exp) {
+                int coef = multi -> coef + temp3 -> coef;
+                if (coef == 0) {
+                    pBIG -> next = temp3 -> next;
+                    ListNode* trash = temp3;
+                    temp3 = temp3 -> next;
+                    delete trash;
+                }
+                else {
+                    temp3 -> coef = coef;
+                }
+            }
+            else {//multi < temp3
+                if (temp3 -> next == poly_end) {
+                    temp3 -> next = multi;
+                }
+                else {
+                    temp3 = temp3 -> next;
+                    pBIG = pBIG -> next;
+                }
+            }
+            
+        }
+        temp1 = temp1 -> next;
+    }
+
+    //for testing
+    temp1 = poly_end -> next;
+    while (temp1 != poly_end) {
+        cout << temp1 -> coef << "x^" << temp1 -> exp << "+";
+        temp1 = temp1 -> next;
+    }
+    
+    
+    return 0;
+}
