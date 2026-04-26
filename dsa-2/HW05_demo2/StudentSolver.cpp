5dd0c33bd158ac4ecf60940abf003b584e1ebed8
Blank <zhizhang808@gmail.com>
2026-04-26 15:05:32 +0800

    HW5 first commit

---
+++ dsa-2/HW05_demo2/StudentSolver.cpp
@@ -0,0 +1,24 @@
+#ifndef STUDENTSOLVER_CPP
+#define STUDENTSOLVER_CPP
+
+#include "Maze.cpp"
+
+bool solveByDFS(const Maze& maze, PathNode*& path) {
+    int n = maze.getN();
+    (void)n;
+    path = nullptr;
+    //TODO 1 : Solve by DFS
+
+    return false;
+}
+
+bool solveByBFS(const Maze& maze, PathNode*& path) {
+    int n = maze.getN();
+    (void)n;
+    path = nullptr;
+    //TODO 2 : Solve by BFS
+
+    return false;
+}
+
+#endif
